a57a151461d68d5ad7a7c8d2092db9550376c5d4
Anna-Tabea Manske <1212tabi1212@gmail.com>
2022-09-01 22:07:49 +0200

    edit modul and drawer function

---
+++ client/src/components/BaseModul.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import {
+	Dialog,
+	DialogPanel,
+	DialogTitle,
+	TransitionChild,
+	TransitionRoot,
+} from "@headlessui/vue";
+import { ref } from "vue";
+
+import type { SnipeitAsset } from "@/api/snipeit";
+
+import BaseButton from "./BaseButton.vue";
+import BaseCard from "./BaseCard.vue";
+
+interface Props {
+	asset: SnipeitAsset;
+}
+
+defineProps<Props>();
+const isOpen = ref(false);
+
+function closeModal() {
+	isOpen.value = false;
+}
+function openModal() {
+	isOpen.value = true;
+}
+</script>
+
+<template>
+	<BaseButton theme="primary" @click="openModal"> Hinzufügen </BaseButton>
+
+	<TransitionRoot appear :show="isOpen" as="template">
+		<Dialog as="div" @close="closeModal" class="relative z-10">
+			<TransitionChild
+				as="template"
+				enter="duration-300 ease-out"
+				enter-from="opacity-0"
+				enter-to="opacity-100"
+				leave="duration-200 ease-in"
+				leave-from="opacity-100"
+				leave-to="opacity-0"
+			>
+				<div class="fixed inset-0 bg-black bg-opacity-50" />
+			</TransitionChild>
+
+			<div class="fixed inset-0 overflow-y-auto">
+				<div class="flex min-h-full items-center justify-center p-4 text-center">
+					<TransitionChild
+						as="template"
+						enter="duration-300 ease-out"
+						enter-from="opacity-0 scale-95"
+						enter-to="opacity-100 scale-100"
+						leave="duration-200 ease-in"
+						leave-from="opacity-100 scale-100"
+						leave-to="opacity-0 scale-95"
+					>
+						<DialogPanel
+							class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+						>
+							<DialogTitle
+								as="h3"
+								class="flex justify-between text-lg font-medium leading-6 text-gray-900"
+							>
+								Material hinzugefügt
+								<p class="text-xs text-teal-600">Rückgängig</p>
+							</DialogTitle>
+
+							<div class="mt-2">
+								<BaseCard
+									theme="checkout"
+									:key="asset.id"
+									:id="asset.id"
+									:name="asset.name"
+									:image="asset.image"
+									:status="asset.status_label.name"
+								></BaseCard>
+							</div>
+
+							<div class="mt-4">
+								<RouterLink to="/reservation/checkout"
+									><BaseButton theme="primary">
+										Suche abschließen
+									</BaseButton></RouterLink
+								>
+								<RouterLink to="/search"
+									><BaseButton theme="secondary">
+										Weiter suchen
+									</BaseButton></RouterLink
+								>
+							</div>
+						</DialogPanel>
+					</TransitionChild>
+				</div>
+			</div>
+		</Dialog>
+	</TransitionRoot>
+</template>
---
+++ client/src/components/ModulCheck.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { XIcon } from "@heroicons/vue/outline";
+import { ref } from "vue";
+
+import type { SnipeitAsset } from "@/api/snipeit";
+
+import BaseButton from "./BaseButton.vue";
+import BaseCard from "./BaseCard.vue";
+
+interface Props {
+	id: number;
+}
+
+const props = defineProps<Props>();
+
+// ALLE ASSETS TEST HEHE
+const options = {
+	headers: {
+		Accept: "application/json",
+		"Content-Type": "application/json",
+	},
+};
+
+const assets = ref<SnipeitAsset[]>();
+fetch("http://localhost:3000/assets?category_id=" + props.id, options)
+	.then(res => res.json())
+	.then((data: { assets: { rows: SnipeitAsset[] } }) => {
+		assets.value = data.assets.rows;
+	})
+
+	.then(() => console.log(assets.value));
+</script>
+
+<template>
+	<div>
+		<BaseButton
+			theme="primary"
+			class="hs-dropdown-toggle inline-flex w-full items-center justify-center rounded-md"
+			data-hs-modal="#hs-vertically-centered-modal"
+		>
+			Hinzufügen
+		</BaseButton>
+
+		<div
+			id="hs-vertically-centered-modal"
+			class="hs-modal fixed top-0 left-0 z-[60] hidden h-full w-full overflow-y-auto overflow-x-hidden"
+		>
+			<div
+				class="m-3 mt-0 flex min-h-[calc(100%-3.5rem)] items-center opacity-0 transition-all ease-out hs-modal-open:mt-7 hs-modal-open:opacity-100 hs-modal-open:duration-500 sm:mx-auto sm:w-full sm:max-w-lg"
+			>
+				<div class="flex w-full flex-col rounded-xl border bg-white shadow-sm">
+					<div class="flex items-center justify-between border-b py-3 px-4">
+						<h3 class="font-bold text-gray-800">Material hinzufügen</h3>
+						<button
+							type="button"
+							class="hs-dropdown-toggle inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-md text-sm text-gray-500 transition-all hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white"
+							data-hs-modal="#hs-vertically-centered-modal"
+						>
+							<span class="sr-only">Schließen</span>
+							<XIcon class="h-4 w-4"></XIcon>
+						</button>
+					</div>
+					<div class="overflow-y-auto p-4">
+						<BaseCard
+							theme="checkout"
+							v-for="asset in assets"
+							:key="asset.id"
+							:id="asset.id"
+							:name="asset.name"
+							:image="asset.image"
+							:status="asset.status_label.name"
+						></BaseCard>
+					</div>
+					<div
+						class="flex flex-col items-center justify-end gap-2 border-t py-3 px-4"
+					>
+						<BaseButton
+							theme="secondary"
+							class="hs-dropdown-toggle rounde inline-flex items-center justify-center gap-2"
+							data-hs-modal="#hs-vertically-centered-modal"
+							>Weitere Materialien suchen</BaseButton
+						>
+						<a
+							class="inline-flex items-center justify-center gap-2 rounded-full bg-teal-600 px-4 py-2 text-center text-sm font-semibold text-white hover:bg-teal-600 focus:outline-none focus:ring-1 focus:ring-teal-200"
+							href="/"
+						>
+							Materialsuche abschließen
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
