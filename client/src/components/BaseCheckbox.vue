486b7301573becc3212317dd97b34c9094d5cc25
Anna-Tabea Manske <1212tabi1212@gmail.com>
2022-09-01 22:10:46 +0200

    Add Base components

---
+++ client/src/components/BaseCheckbox.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="mb-2 flex px-4">
+		<input
+			type="checkbox"
+			class="mt-0.5 shrink-0 rounded border-gray-200 text-teal-600 focus:ring-teal-500"
+			id="hs-checkbox-group-1"
+		/>
+		<label for="hs-checkbox-group-1" class="ml-3 text-sm text-gray-500"
+			><slot></slot
+		></label>
+	</div>
+</template>
---
+++ client/src/components/BaseDateTimePicker.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useDateStore } from "@/stores/date";
+
+const dateStore = useDateStore();
+</script>
+
+<template>
+	<div>
+		<v-date-picker
+			mode="dateTime"
+			v-model="dateStore.$state"
+			:min-date="new Date()"
+			is-range
+			is24hr
+			:valid-hours="{ min: 9, max: 16 }"
+			:minute-increment="5"
+			is-expanded
+			color="teal"
+			:disabled-dates="{ weekdays: [1, 7] }"
+		/>
+		<div class="mt-2 flex">
+			<!-- <span class="w-20 font-semibold text-gray-600">Abholung:</span>
+			<span class="ml-2">{{ dateStore.formattedStart }}</span> -->
+		</div>
+		<div class="mt-2 flex">
+			<!-- <span class="w-20 font-semibold text-gray-600">Rückgabe:</span>
+			 <span class="ml-2">{{ dateStore.formattedEnd }}</span> -->
+		</div>
+	</div>
+</template>
