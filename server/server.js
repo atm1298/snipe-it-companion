95aface24c919c105e84e085119c0b88ee5b1156
Anna-Tabea Manske <1212tabi1212@gmail.com>
2022-09-01 22:14:02 +0200

    edit server add snipeit

--- server/server.js
+++ server/server.js
@@ -1,20 +1,66 @@
 // Require the framework and instantiate it
+import 'dotenv/config';
+import fetch from 'node-fetch';
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
 
-// CommonJs
-const fastify = require('fastify')({
+const server = Fastify({
 	logger: true,
 });
+server.register(cors);
 
-// Declare a route
-fastify.get('/', function (request, reply) {
-	reply.send({ hello: 'world' });
+// ALLE ASSETS TEST HEHE
+const options = {
+	method: 'GET',
+	headers: {
+		Accept: 'application/json',
+		Authorization: 'Bearer ' + process.env.API_KEY,
+	},
+};
+
+// Hardware
+server.get('/assets', async function (request, reply) {
+	const searchParams = new URLSearchParams(request.query);
+	const assets = await fetch(
+		'https://snipeit.imis.uni-luebeck.de/api/v1/hardware?' +
+			searchParams.toString(),
+		options
+	)
+		.then((response) => response.json())
+		.catch((err) => console.error(err));
+	//console.log(assets);
+	return { assets };
+});
+
+// EINE Hardware mit ID für Details
+server.get('/assets/:id', async function (request, reply) {
+	const { id } = request.params;
+	const asset = await fetch(
+		'https://snipeit.imis.uni-luebeck.de/api/v1/hardware/' + id,
+		options
+	)
+		.then((response) => response.json())
+		.catch((err) => console.error(err));
+	console.log(asset);
+	return { asset };
+});
+
+// Kategorien
+server.get('/categories', async function (request, reply) {
+	const categories = await fetch(
+		'https://snipeit.imis.uni-luebeck.de/api/v1/categories?sort=name&order=asc',
+		options
+	)
+		.then((response) => response.json())
+		.catch((err) => console.error(err));
+	console.log(categories);
+	return { categories };
 });
 
 // Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
+server.listen({ port: 3000 }, function (err, address) {
 	if (err) {
-		fastify.log.error(err);
+		server.log.error(err);
 		process.exit(1);
 	}
-	// Server is now listening on ${address}
 });
