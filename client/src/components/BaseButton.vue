c1a84ed95c346ef2eb5684e4797ea7847dcd0b38
Anna-Tabea Manske <1212tabi1212@gmail.com>
2022-09-09 18:23:22 +0200

    edit layout and look

--- client/src/components/BaseButton.vue
+++ client/src/components/BaseButton.vue
@@ -9,11 +9,11 @@ defineProps<Props>();
 <template>
 	<button
 		type="button"
-		class="rounded-xl px-4 py-2 text-center text-sm font-semibold focus:outline-none focus:ring-1"
+		class="rounded-xl px-4 py-3 text-center text-sm font-semibold focus:outline-none focus:ring-1"
 		:class="{
-			'bg-gray-600 text-white hover:bg-gray-600 focus:ring-gray-200':
+			'bg-orange-500 text-white hover:bg-orange-400 focus:ring-orange-200':
 				theme == 'primary',
-			'bg-gray-50 text-gray-600 hover:bg-gray-300 focus:ring-gray-200':
+			'bg-orange-100 text-orange-800 hover:bg-orange-200 focus:ring-orange-200':
 				theme == 'secondary',
 		}"
 	>
