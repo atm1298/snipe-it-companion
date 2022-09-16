878710ccd41d1054ba7d442051c7bf50721839cd
Anna-Tabea Manske <1212tabi1212@gmail.com>
2022-09-16 14:38:22 +0200

    delete Base components

--- client/src/components/BaseDateRangePicker.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { ref } from "vue";
-
-const range = ref({ start: new Date(2022, 7, 22), end: new Date(2022, 7, 22) });
-</script>
-
-<template>
-	<v-date-picker v-model="range" is-range color="orange" :rows="2" is-expanded />
-</template>
--- client/src/pages/reservation/upcoming.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import DateRange from "@/components/DateRange.vue";
-</script>
-
-<template>
-	<div>
-		<DateRange></DateRange>
-	</div>
-</template>up
