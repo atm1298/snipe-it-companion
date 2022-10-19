d90e3605269bde6b0e7ba24a08a65f89df03bca0
Anna-Tabea Manske <1212tabi1212@gmail.com>
2022-10-19 07:43:53 +0200

    change asset dir

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
 		},
 	},
+	build: {
+		assetsDir: "stuff",
+	},
 	server: {
 		cors: true,
 		host: "localhost",
